fa086c1ebaf88c76b0133ffbe449720454516b9c
kenjitheman <kenjitheman@proton.me>
2023-09-23 15:34:37 +0300

    chore: remove try funcs

--- dataman/htable.go
+++ dataman/htable.go
@@ -20,20 +20,3 @@ func (ht *HashTable) Get(key string) (int, bool) {
 func (ht *HashTable) Delete(key string) {
 	delete(ht.data, key)
 }
-
-// func TryHT() {
-// 	ht := NewHashTable()
-// 	ht.Put("Alice", 25)
-// 	ht.Put("Bob", 30)
-//
-// 	age, found := ht.Get("Alice")
-// 	if found {
-// 		fmt.Printf("Alice's age is %d\n", age)
-// 	}
-//
-// 	ht.Delete("Bob")
-// 	_, found = ht.Get("Bob")
-// 	if !found {
-// 		fmt.Println("Bob not found")
-// 	}
-// }
--- sort/selection.go
+++ sort/selection.go
@@ -5,7 +5,7 @@ func SelectionSort(arr []int) {
 	for i := 0; i < n-1; i++ {
 		minIdx := i
 		for j := i + 1; j < n; j++ {
-			if arr[j] < arr[minIdx] {
+			if arr[minIdx] > arr[j] {
 				minIdx = j
 			}
 		}
