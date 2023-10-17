d16eb8a547faf609f1ef83f9bc0302c1e2669018
kenjitheman <kenjith3man@gmail.com>
2023-10-17 22:24:03 +0300

    feat: Add .gitignore Closes #9

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
+	text, err := print_some_value()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println(text)
+}
+
+func print_some_value() (string, error) {
+	return "some value", nil
 }
