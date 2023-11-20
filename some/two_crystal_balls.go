cbf338c579c101c212f0f542d2694d5a62014fa2
kenjitheman <kenjith3man@gmail.com>
2023-11-20 22:43:08 +0200

    chore: Add some todos Closes #18

--- some/two_crystal_balls.go
+++ some/two_crystal_balls.go
@@ -1,19 +1,19 @@
 package some
 
 import (
-    "math"
+	"math"
 )
 
 func TwoCrystalBalls(breaks []bool) int {
-    stepSize := int(math.Floor(math.Sqrt(float64(len(breaks)))))
-    i := stepSize
-    for i < len(breaks) && !breaks[i] {
-        i += stepSize
-    }
-    for j := i - stepSize; j < i; j++ {
-        if breaks[j] {
-            return j
-        }
-    }
-    return -1
+	stepSize := int(math.Floor(math.Sqrt(float64(len(breaks)))))
+	i := stepSize
+	for i < len(breaks) && !breaks[i] {
+		i += stepSize
+	}
+	for j := i - stepSize; j < i; j++ {
+		if breaks[j] {
+			return j
+		}
+	}
+	return -1
 }
