694611895a99105d30775c5093eb50048a4bb8c3
kenjitheman <btwdesu@gmail.com>
2024-03-03 21:24:00 +0200

    chore: update some

--- go/main.go
+++ go/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/plastiey/codegen/problems"
+import (
+	"github.com/plastiey/codegen/problems"
+)
 
 func main() {
 	var nums = []int{2, 7, 7, 1, 3, 3, 2, 11, 15, 8, 4, 9, 6, 5, 10, 12, 13, 14}
